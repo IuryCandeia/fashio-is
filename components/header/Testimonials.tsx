1a6e12b17b0005090683684a519fd55840ceb61f
Linely Candeia <iurycfmorais@gmail.com>
2023-05-20 13:21:22 -0300

    add testimonials 2.0

---
+++ components/header/Testimonials.tsx
@@ -0,0 +1,18 @@
+export interface Props {
+  title: string;
+  subtitle: string;
+}
+
+export default function Testemonials({
+    title ,
+    subtitle ,
+    // imageUrl = "https://deco.cx/images/deco-logo-light.png",
+  }: Props) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h1>{subtitle}</h1>
+        </div>
+    );
+    
+}
