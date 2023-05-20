ccab46885bb459fb69e2d7d5d169413e2d456eeb
Linely Candeia <iurycfmorais@gmail.com>
2023-05-20 13:21:49 -0300

    ok

--- components/header/Testimonials.tsx
+++ components/header/Testimonials.tsx
@@ -4,15 +4,14 @@ export interface Props {
 }
 
 export default function Testemonials({
-    title ,
-    subtitle ,
-    // imageUrl = "https://deco.cx/images/deco-logo-light.png",
-  }: Props) {
-    return (
-        <div>
-            <h1>{title}</h1>
-            <h1>{subtitle}</h1>
-        </div>
-    );
-    
+  title,
+  subtitle,
+  // imageUrl = "https://deco.cx/images/deco-logo-light.png",
+}: Props) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h1>{subtitle}</h1>
+    </div>
+  );
 }
